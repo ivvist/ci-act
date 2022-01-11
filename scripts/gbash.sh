986fbbc6d41d18fce6114d29f1e7a284ee108cbe
ivvist <ivvist@gmail.com>
2022-01-11 11:14:14 +0300

    misc

--- scripts/gbash.sh
+++ scripts/gbash.sh
@@ -9,7 +9,7 @@ echo "Install golangci-lint in folder: $1/bin"
 curl -sfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh| sh -s -- -b $1/bin v1.43.0
 
 echo "Download golangci config"
-curl -s https://raw.githubusercontent.com/untillpro/ci-action/master/scripts/.golangci.yml > /home/runner/work/core-iblobstoragestg/.golangci.yml
+curl -s https://raw.githubusercontent.com/untillpro/ci-action/master/scripts/.golangci.yml > /home/runner/work/.golangci.yaml
 
 echo "Run linter jobs"
 $1/bin/golangci-lint run -v    
